d354bd3f259131acbae559fc720f15710dce7363
KasoLu <kasoalpha@gmail.com>
2019-12-14 22:50:03 +0800

    fix -- adjust uniquify pass

--- uniquify.rkt
+++ uniquify.rkt
@@ -5,12 +5,11 @@
 
 (define ast:pgm
   (match-lambda
-    [`(program ,pi ,def* ... ,expr)
-      (let ([def/u* (map ast:def/dec def*)])
-        (let ([f* (map define->name def*)] [f/u* (map define->name def/u*)])
-          (let ([v.u* (map make-pair f* f/u*)])
-           `(program ,pi ,@(map (curry ast:def/exp v.u*) def/u*)
-             ,(ast:exp v.u* expr)))))]))
+    [`(program ,pi . ,def+)
+      (let ([def/u+ (map ast:def/dec def+)])
+        (let ([f+ (map define->name def+)] [f/u+ (map define->name def/u+)])
+          (let ([v.u+ (map make-pair f+ f/u+)])
+           `(program ,pi . ,(map (curry ast:def/exp v.u+) def/u+)))))]))
 
 (define ast:def/dec
   (match-lambda
@@ -18,33 +17,33 @@
      `(define (,(gensym f) . ,v*) ,e)]))
 
 (define ast:def/exp
-  (lambda (v.u* def)
+  (lambda (v.u+ def)
     (match def
       [`(define (,f . ,v*) ,e)
         (let ([vu* (map gensym v*)])
          `(define (,f . ,vu*)
-           ,(ast:exp (assoc-add v.u* (map make-pair v* vu*)) e)))])))
+           ,(ast:exp (assoc-add v.u+ (map make-pair v* vu*)) e)))])))
 
 (define ast:exp
-  (lambda (v.u* expr)
-    (let ([recur (curry ast:exp v.u*)])
+  (lambda (v.u+ expr)
+    (let ([recur (curry ast:exp v.u+)])
       (match expr
         [`(typed ,e ,t)
          `(typed ,(recur e) ,t)]
         [`(let ([,v ,e1]) ,e2)
           (let ([v/u (gensym v)])
            `(let ([,v/u ,(recur e1)])
-             ,(ast:exp (assoc-add v.u* v v/u) e2)))]
+             ,(ast:exp (assoc-add v.u+ v v/u) e2)))]
         [`(lambda ,v* ,e1)
           (let ([v/u* (map gensym v*)])
-           `(lambda ,v/u* ,(ast:exp (assoc-add v.u* (map make-pair v* v/u*)) e1)))]
+           `(lambda ,v/u* ,(ast:exp (assoc-add v.u+ (map make-pair v* v/u*)) e1)))]
         [`(,op . ,e*)
          `(,op . ,(map recur e*))]
-        [_(ast:arg v.u* expr)]))))
+        [_(ast:arg v.u+ expr)]))))
 
 (define ast:arg
-  (lambda (v.u* arg)
+  (lambda (v.u+ arg)
     (match arg
-      [(? symbol?) (assoc-ref v.u* arg)]
+      [(? symbol?) (assoc-ref v.u+ arg)]
       [(else) arg])))
 
